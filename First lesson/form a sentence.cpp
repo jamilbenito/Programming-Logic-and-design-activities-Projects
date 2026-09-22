1ef7ea6ba3491accaa31816ed4b6e57c6c8070c9
jamilbenito <152799319+jamilbenito@users.noreply.github.com>
2026-09-22 13:43:34 +0800

    Add files via upload

---
+++ First lesson/form a sentence.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string first_name = "Jamil Benito";
+	string last_name = "Inocencio ";
+	string program = "BS Computer Engineering";
+	string section = "1A";
+	string address = "San Vicente, Baao, Camarines Sur";
+	int age = 19;
+	string gender = "Male";
+	
+	cout <<"Hi my Name is " 
+	<< first_name 
+	<< " " 
+	<< last_name 
+	<< "iam " 
+	<< age 
+	<< " years old "
+	<< "i live in " 
+	<< address 
+	<< " my program is " 
+	<< program 
+	<< " Section " 
+	<< section 
+	<< " my gender is " 
+	<< gender 
+	<< "." 
+	<<endl;
+}
+
---
+++ First lesson/variable_declaration.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+				
+	int x = 100;
+	float pi = 3.1416;
+	double num = 1564.7655;
+	char let = 'a';
+	bool isEven = true;
+	string name = "Jamil Benito";
+	
+	cout<<"The value of x is: "<< x<< endl;		
+	cout<< pi<<endl;
+	cout << num << endl;
+	cout << let << endl;
+	cout << isEven << endl;
+	cout << name << endl;
+
+}
