62dafbc659121f5ddd08c2f50b5aec1ea72c1281
jamilbenito <152799319+jamilbenito@users.noreply.github.com>
2026-09-22 14:05:06 +0800

    Add files via upload

---
+++ First lesson/mathematical operations.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int x = 1000;
+	int y = 200;
+	
+	cout<<"The sum of x and y is " << x + y <<endl;
+	cout<<"The difference of x and y is " << x - y <<endl;
+	cout<<"The product of x and y is " << x * y <<endl;
+	cout<<"The quotient of x and y is "<< x / y <<endl;
+}
